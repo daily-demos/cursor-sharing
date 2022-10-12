b6b45b0e4478ec64dd8f3f0233afdb8e1a0094b7
Liza Shulyayeva <65890040+lazeratops@users.noreply.github.com>
2022-10-12 13:18:12 +0200

    Rename *Div suffix to *Element

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import joinCall from './call.js';
 import { showCall } from './scenes.js';
 
 window.addEventListener('DOMContentLoaded', () => {
-  // Setup entry
+  // Set up entry
   const form = document.getElementById('enterCall');
   form.onsubmit = (ev) => {
     ev.preventDefault();
